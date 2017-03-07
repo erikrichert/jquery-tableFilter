af982a56b6a5da83c54f343ae36d87e9b47083f0
Erik Richert <erichert@unm.edu>
2017-03-07 15:45:41 -0700

    fix checking of caseSensitive option

--- jquery.tableFilter.js
+++ jquery.tableFilter.js
@@ -2,15 +2,16 @@
     "use strict";
     $.fn.tableFilter = function(options) {
 	
-    var settings = $.extend({
-		tableID: '#filter-table',
-		filterID: '#filter',
-		filterCell: '.filter-cell',
-		autofocus: false,
-		caseSensitive: true,
-		noresults: 'no results found',
-		columns: null
-	}, options);
+		//default settings
+	    var settings = $.extend({
+			tableID: '#filter-table',
+			filterID: '#filter',
+			filterCell: '.filter-cell',
+			autofocus: false,
+			caseSensitive: false,
+			noresults: 'no results found',
+			columns: null
+		}, options);
 		
 		//auto focus on filter element if autofocs set to true
 		if(settings.autofocus) {
@@ -22,13 +23,16 @@
 			settings.columns = $(settings.tableID + ' > tbody > tr:first >td').length;
 		}
 		
-		if(settings.caseSensitive) {
+		var contains = ':contains';
+		
+		if(!settings.caseSensitive) {
 			//create custom "icontains" selector for case insensitive search
 			$.expr[':'].icontains = $.expr.createPseudo(function(text) {
 			    return function(e) {
 			        return $(e).text().toUpperCase().indexOf(text.toUpperCase()) >= 0;
 			    };
 			});
+			contains = ':icontains';
 		}
 		
 		return this.find(settings.filterID).on("change paste keyup", function() {
@@ -37,7 +41,7 @@
 		
 			//for each student name compare versus filter input
 			$(settings.filterCell).each(function(i){ //pass i as iterator
-				if($(this).is(':icontains(' + filterString + ')')) {
+				if($(this).is(contains + '(' + filterString + ')')) {
 					//check hidden rows for backspace operation
 					if($(this).is(':hidden')) {
 						$(this).parent().removeClass('filter-hidden').show();
