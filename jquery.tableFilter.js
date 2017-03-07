9708ed96807149a33b480b76fa0d6e186387689b
Erik Richert <erichert@unm.edu>
2017-03-07 14:20:58 -0700

    initial commit

---
+++ jquery.tableFilter.js
@@ -0,0 +1,68 @@
+(function($){
+	$.fn.tableFilter = function(options) {
+		
+		var settings = $.extend({
+			tableID: '#filter-table',
+			filterID: '#filter',
+			filterCell: '.filter-cell',
+			autofocus: false,
+			caseSensitive: true,
+			noresults: 'no results found',
+			columns: null
+		}, options);
+		
+		//auto focus on filter element if autofocs set to true
+		if(settings.autofocus) {
+			$(settings.filterID).focus();
+		}
+		
+		var rowCount = $(settings.filterCell).parent().length;
+		if(settings.columns === null) {
+			settings.columns = $(settings.tableID + ' > tbody > tr:first >td').length;
+		}
+		
+		if(settings.caseSensitive) {
+			//create custom "icontains" selector for case insensitive search
+			$.expr[':'].icontains = $.expr.createPseudo(function(text) {
+			    return function(e) {
+			        return $(e).text().toUpperCase().indexOf(text.toUpperCase()) >= 0;
+			    };
+			});
+		}
+		
+		return this.find(settings.filterID).on("change paste keyup", function() {
+			//get value of input
+			var filterString = $(this).val();
+		
+			//for each student name compare versus filter input
+			$(settings.filterCell).each(function(i){ //pass i as iterator
+				if($(this).is(':icontains(' + filterString + ')')) {
+					//check hidden rows for backspace operation
+					if($(this).is(':hidden')) {
+						$(this).parent().removeClass('filter-hidden').show()
+					}
+				} else {
+					$(this).parent().addClass('filter-hidden').hide();
+				}
+				//check if .each() iterations complete
+				if(rowCount == (i + 1)) {
+					//find rows with 'hidden' class and compare to row count if equal then display 'no results found' message
+					var hidden = $(settings.tableID).find('.filter-hidden').length;
+					if (hidden == rowCount) {
+						if ($('#noResults').is(':visible')){ //do not display multiple "no results" messages
+							return;
+						} else {
+							var newRow = $('<tr id="noResults"><td colspan="' + settings.columns +'"><em>' + settings.noresults + '</em></td></tr>').hide(); //row can be styled with CSS
+							$(settings.tableID).append(newRow);
+							newRow.show();
+						}
+					} else if ($('#noResults').is(':visible')){
+						$('#noResults').remove();
+					} else {
+						return;
+					}
+				}
+			}); 
+		});
+	};
+}(jQuery));
